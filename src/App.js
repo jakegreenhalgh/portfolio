1c3b4ba370ce17f29ffd0febf7801afb7f434fd5
Jacob <jacob_greenhalgh@hotmail.com>
2022-09-01 18:16:31 +0100

    added fonts and background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="Home">
-     <h1>Jacob Greenhalgh</h1>
+     <h1 className="Title">Jacob Greenhalgh</h1>
       <h2>My Projects:</h2>
         <h3>Education Application (Group Project)</h3>
           <a href='https://github.com/jakegreenhalgh/group_project_education_app'>https://github.com/jakegreenhalgh/group_project_education_app</a>
