becdb0bf04cbc4f77196589c8f31943cd85eb1b3
Jacob <jacob_greenhalgh@hotmail.com>
2022-09-01 10:35:58 +0100

    added portfolio links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <p>Hello</p>
+     <h1>Jacob Greenhalgh</h1>
+     <h2>My Projects:</h2>
+      <h3>Education Application (Group Project)</h3>
+        <a href='https://github.com/jakegreenhalgh/group_project_education_app'>https://github.com/jakegreenhalgh/group_project_education_app</a>
+      <h3>Vet Management System</h3>
+        <a href='https://github.com/jakegreenhalgh/vet_manager'>Vet Manager</a>
     </div>
   );
 }
