86503b8bd42d62ec50c2ccfccc5a82cb28392998
Jacob <jacob_greenhalgh@hotmail.com>
2022-09-01 12:54:42 +0100

    added favicon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Home">
      <h1>Jacob Greenhalgh</h1>
-     <h2>My Projects:</h2>
-      <h3>Education Application (Group Project)</h3>
-        <a href='https://github.com/jakegreenhalgh/group_project_education_app'>https://github.com/jakegreenhalgh/group_project_education_app</a>
-      <h3>Vet Management System</h3>
-        <a href='https://github.com/jakegreenhalgh/vet_manager'>Vet Manager</a>
+      <h2>My Projects:</h2>
+        <h3>Education Application (Group Project)</h3>
+          <a href='https://github.com/jakegreenhalgh/group_project_education_app'>https://github.com/jakegreenhalgh/group_project_education_app</a>
+        <h3>Vet Management System</h3>
+          <a href='https://github.com/jakegreenhalgh/vet_manager'>https://github.com/jakegreenhalgh/vet_manager</a>
     </div>
   );
 }
